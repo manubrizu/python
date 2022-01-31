17cc8490a18fc6ece959e65a5e14f0485d4b68d7
manubrizu <manuel.brizuela@philips.edu.ar>
2022-01-30 22:44:12 -0300

    Delete 1_number_guessing.py

--- Nueva carpeta/1_number_guessing.py
+++
@@ -1,9 +0,0 @@
-while True:
-    number = 15
-    guess = 0
-    print("Guess the number from 1-20")
-    guess = input()
-    guess = int(guess)
-    if guess == number:
-        print("Well done!")
-        break
